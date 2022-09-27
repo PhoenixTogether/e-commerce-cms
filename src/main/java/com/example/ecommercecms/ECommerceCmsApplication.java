746401eb957d906a72e704f2f521a14044adf547
Jiyong Sim <sjy19910222@gmail.com>
2022-09-27 19:17:51 +0900

    Initial commit

---
+++ src/main/java/com/example/ecommercecms/ECommerceCmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommercecms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceCmsApplication.class, args);
+    }
+
+}
